373e2adfae67d2335dc178aebdc26d4067ef714b
Chen9818 <88109684+Chen9818@users.noreply.github.com>
2022-02-10 00:42:05 +0800

    add google map and swiper

--- products.js
+++ products.js
@@ -1,3 +1,4 @@
+// import {Modal} from "./node_modules/bootstrap/dist/js/bootstrap";
 function hideLoader() {
     $('#loading').fadeOut('slow');
 }
@@ -6,3 +7,15 @@ $(window).on("load",hideLoader);
 
 // Strongly recommended: Hide loader after 20 seconds, even if the page hasn't finished loading
 setTimeout(hideLoader, 20 * 1000);
+
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  
+
